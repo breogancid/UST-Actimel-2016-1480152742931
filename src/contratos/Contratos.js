044bd4693c6f0dea83062ba53c35c69245487fa5
Carlos Manchon <yomanch@hotmail.com>
2016-11-26 08:07:10 -0600

    Añado nuevo tipo de contrato

--- src/contratos/Contratos.js
+++ src/contratos/Contratos.js
@@ -53,5 +53,5 @@ contract Permission {
 
    
 }*/
-
+var contratoRecurso = 'contract Resource {function entrarOK(uint user) returns (bool) {return true;}function entrarNOT(uint user) returns (bool){return false;}}';
 var contratoPermisos = 'contract Permission { mapping (uint => uint[]) public permissionsResources; mapping (uint => uint) public numPermissionsPerResource; uint numUsers; uint numResources; uint[] users; uint[] resources; // This is the constructor whose code is // run only when the contract is created. function Permission() { delete numUsers; delete numResources; delete users; delete resources; } function createUser(uint user){ numUsers++; users[numUsers] = user; } function createResource(uint resource){ numResources++; resources[numResources] = resource; numPermissionsPerResource[resource] = 0; } function addPermission(uint user,uint resource){ uint[] resourcePermission = permissionsResources[resource]; uint users = numPermissionsPerResource[resource]; resourcePermission[users] = user; users++; numPermissionsPerResource[resource] = users; } //function deletePermission(uint user,uint resource) //function deleteUser //function deleteResource }';
