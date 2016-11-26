858f8b25a6bf30253d41a4e85d7ee6cb1a4d030e
Alberto Fernandez Gonzalez <alberto.f@es.ibm.com>
2016-11-26 09:06:28 -0600

    sorry breo

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/*eslint-env node*/
+/*eslint-env node, pg*/
 
 //------------------------------------------------------------------------------
 // node.js starter application for Bluemix
@@ -12,6 +12,9 @@ var express = require('express');
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
+var pg = require("pg");
+
+var credentials = {"uri":"postgre://uqhvzfad:nPuFDYCqAAnkVtXZTnO7818hw9KQgp_s@jumbo.db.elephantsql.com:5432/uqhvzfad"};
 // create a new express server
 var app = express();
 var bodyParser = require('body-parser');
@@ -34,11 +37,33 @@ app.post('/enviardatos', function (req, res) {
     res.send(pagina);    
     
     console.log("recibido " + msj1 + msj2);
+});
+
+app.post('/consulta',function (req, res) {
+  var client = new pg.Client(credentials.uri);
+
+
+    var pagina='<!doctype html> <html><head></head><body>';
+    
+client.connect(function(err) {
+    if (err) {
+      res.end("Could not connect to postgre: " + err);
+      console.log("mierda");
+    }
+    client.query('SELECT NOW() AS "pgTime"', function(err, result) {
+      if (err) {
+        res.end("Error running query: " + err);
+        console.log("medio bien");
+      }
+      res.end("PG Time: " + result.rows[0].pgTime);
+              console.log("perfe illo PG Time:" + result.rows[0].pgTime);
+
+      client.end();
+
+      
+    })
 })
-
-
-
-
+});
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
---
+++ appPG.js
@@ -0,0 +1,28 @@
+var pg = require("pg")
+
+
+if (process.env.VCAP_SERVICES) {
+  var env = JSON.parse(process.env.VCAP_SERVICES);
+  var credentials = env['postgresql-9.1'][0]['credentials'];
+} else {
+  var credentials = {"uri":"postgre://user:secret1@localhost:5433/db"}
+}
+
+var port = (process.env.VCAP_APP_PORT || 1337);
+var host = (process.env.VCAP_APP_HOST || '0.0.0.0');
+
+http.createServer(function(req, res) {
+  var client = new pg.Client(credentials.uri);
+  client.connect(function(err) {
+    if (err) {
+      res.end("Could not connect to postgre: " + err);
+    }
+    client.query('SELECT NOW() AS "pgTime"', function(err, result) {
+      if (err) {
+        res.end("Error running query: " + err);
+      }
+      res.end("PG Time: " + result.rows[0].pgTime);
+      client.end();
+    });
+  });
+}).listen(port, host);
