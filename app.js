4f6a702d2d7469ef32dfc5948a82383921c7a602
Alberto Fernandez Gonzalez <alberto.f@es.ibm.com>
2016-11-26 06:24:33 -0600

    Desde PoC1 se envia datos y se ven en web y console log

--- app.js
+++ app.js
@@ -14,12 +14,31 @@ var cfenv = require('cfenv');
 
 // create a new express server
 var app = express();
+var bodyParser = require('body-parser');
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
-
+app.use(bodyParser.urlencoded({ extended: false }));
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
+
+app.post('/enviardatos', function (req, res) {
+    var msj1=req.body.mensaje1;
+    var msj2=req.body.mensaje2;
+
+	
+    var pagina='<!doctype html>mensaje enviado<html><head></head><body>';
+
+        pagina += msj1 + msj2;
+
+    res.send(pagina);    
+    
+    console.log("recibido " + msj1 + msj2);
+})
+
+
+
+
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
